1b581effbbc1b3a6a0167b2aaad14b1e86285366
Anurag Sachan <nrg.sachan@gmail.com>
2020-08-23 23:04:08 +0530

    Find kth smallest element in an Array

---
+++ DS_Algo/FindKthSmallest.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Main
+{
+	// Function to find the K'th smallest element in the
+	// array using max-heap
+	public static int findKthSmallest(List<Integer> A, int k)
+	{
+		// create a max-heap using PriorityQueue class and
+		// insert first k elements of the array into the heap
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+	
+		// do for remaining array elements
+		for (int i = 0; i < A.size(); i++)
+		{
+			pq.add(A.get(i));
+			
+			// if PriorityQueue's size is greater than k
+			if (pq.size() > k) {
+				pq.poll();
+			}
+		}
+
+		// return the root of max-heap
+		return pq.peek();
+	}
+
+	public static void main(String[] args)
+	{
+		List<Integer> A = Arrays.asList(7, 4, 6, 3, 9, 1);
+		int k = 3;
+
+		System.out.println("K'th smallest element in the array is " +
+									findKthSmallest(A, k));
+	}
+}
